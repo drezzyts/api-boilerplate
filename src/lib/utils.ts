7fc95f7e46f24536357aa254dbb628fdd1be215f
drezzyts <drezzydev@gmail.com>
2024-10-04 23:34:40 -0300

    fix: fixing some typing mistakes.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import type { UseCase } from "../typings/utils";
 import type { BaseGateway } from "../typings/gateways";
 
-export function createUseCase<I, O, R extends BaseGateway>(fn: UseCase<I, O, R>) {
+export function createUseCase<I, O, R extends BaseGateway<unknown>>(fn: UseCase<I, O, R>) {
     return fn;
 }
--- src/typings/utils.ts
+++ src/typings/utils.ts
@@ -1,4 +1,4 @@
 import { BaseGateway } from "./gateways";
 
-export type UseCase<Input, Output, Repository extends BaseGateway> = 
+export type UseCase<Input, Output, Repository extends BaseGateway<unknown>> = 
     (input: Input, repository: Repository) => Promise<Output>; 
